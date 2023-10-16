c5c21fe66a5755be67dd8b2e87ef9b11ace5278d
NameUserEnter <tomredl2003@gmail.com>
2023-10-16 19:28:28 +0300

    Add API methods: GetEvent, GetEventsByFilter, PutEvent, PostEvent, DeleteEvent

--- MapOfActivitiesAPI/Controllers/EventController.cs
+++ MapOfActivitiesAPI/Controllers/EventController.cs
@@ -1,6 +1,8 @@
 ﻿using MapOfActivitiesAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Drawing;
 
 namespace MapOfActivitiesAPI.Controllers
 {
@@ -18,6 +20,97 @@ namespace MapOfActivitiesAPI.Controllers
         public IEnumerable<Event> Get()
         {
             return _context.Events;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Event>> GetEvent(int id)
+        {
+            if (_context.Events == null)
+            {
+                return NotFound();
+            }
+            var confectioner = await _context.Events.FindAsync(id);
+
+            if (confectioner == null)
+            {
+                return NotFound();
+            }
+
+            return confectioner;
+        }
+
+        [HttpGet("filter")]
+        public IEnumerable<Event> GetEventsByFilter(string filter)
+        {
+            var filterType = _context.Types.Where(x => x.Name == filter).Select(x=>x.Id).FirstOrDefault();
+            var points = _context.Events.AsQueryable();
+
+            if (!string.IsNullOrEmpty(filter))
+            {
+                points = points.Where(p => p.TypeId == filterType);
+            }
+
+            return (IEnumerable<Event>)points.ToList();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEvent(int id, Event myEvent)
+        {
+            if (id != myEvent.Id)
+            {
+                return BadRequest();
+            }
+            else{ }
+            _context.Entry(myEvent).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if ((_context.Events?.Any(e => e.Id == id)).GetValueOrDefault())
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Event>> PostEvent(Event myEvent)
+        {
+            if (_context.Events == null)
+            {
+                return Problem("Entity set 'MapOfActivitiesAPIContext.Events'  is null.");
+            }
+            _context.Events.Add(myEvent);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteConfectioner(int id)
+        {
+            if (_context.Events == null)
+            {
+                return NotFound();
+            }
+            var myEvent = await _context.Events.FindAsync(id);
+            if (myEvent == null)
+            {
+                return NotFound();
+            }
+            _context.Events.Remove(myEvent);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
