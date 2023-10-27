2b4ece22087c529072cd07041796af3b4661a7b6
Oleksii Matsui <ommatsui@knu.ua>
2023-10-27 12:43:46 +0300

    fix: make tymespan nullable

--- MapOfActivitiesAPI/Models/Type.cs
+++ MapOfActivitiesAPI/Models/Type.cs
@@ -9,7 +9,7 @@ namespace MapOfActivitiesAPI.Models
             public int ParentTypeId { get; set; }
             public string Name { get; set; }
             public string? ImageURL { get; set; }
-            public TimeSpan MaxDuration { get; set; }
+            public TimeSpan? MaxDuration { get; set; }
             public List<Event>? Events { get; set; }
             public List<Type>? Children { get; set; }
     }    
