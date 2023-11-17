0dc8d07d7a5ab2ccd80e0cee1bcdbc3e49fc901f
OleksiiA <astrakhantsev@sunpoint.com.ua>
2023-11-17 23:36:06 +0200

    combined User and UserProfile controllers , add method get roles

--- MapOfActivitiesAPI/Controllers/UserController.cs
+++ MapOfActivitiesAPI/Controllers/UserController.cs
@@ -13,15 +13,17 @@ public class UserController : ControllerBase
 {
     private readonly UserManager<ApplicationUser> _userManager;
     private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly MapOfActivitiesAPIContext _context;
 
-    public UserController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+    public UserController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, MapOfActivitiesAPIContext context)
     {
         _userManager = userManager;
         _roleManager = roleManager;
+        _context = context;
     }
 
     [HttpGet]
-    [Route("users")]
+    [Route("get-users-with-roles")]
     public async Task<IActionResult> GetUsersWithRoles()
     {
         var users = await _userManager.Users.ToListAsync();
@@ -43,9 +45,8 @@ public class UserController : ControllerBase
         return Ok(usersWithRoles);
     }
 
-
     [HttpGet]
-    [Route("users/{userId}")]
+    [Route("get-user/{userId}")]
     public async Task<IActionResult> GetUser(string userId)
     {
         var user = await _userManager.FindByIdAsync(userId);
@@ -54,18 +55,20 @@ public class UserController : ControllerBase
         {
             return NotFound();
         }
+        var role = await _userManager.GetRolesAsync(user);
 
         var userData = new
         {
             user.Id,
-            user.Email
+            user.Email,
+            Role = role
         };
 
         return Ok(userData);
     }
 
     [HttpPost]
-    [Route("banned/{userId}")]
+    [Route("add-banned/{userId}")]
     public async Task<IActionResult> AddBanned(string userId)
     {
         var user = await _userManager.FindByIdAsync(userId);
@@ -91,9 +94,8 @@ public class UserController : ControllerBase
         return Ok(new { Message = "User banned successfully." });
     }
 
-
     [HttpDelete]
-    [Route("delete-user/{userId}")]
+    [Route("delete-user-and-profile/{userId}")]
     public async Task<IActionResult> DeleteUser(string userId)
     {
         var user = await _userManager.FindByIdAsync(userId);
@@ -101,6 +103,14 @@ public class UserController : ControllerBase
         if (user == null)
         {
             return NotFound();
+        }
+
+        var userProfile = await _context.Users.FirstOrDefaultAsync(u => u.UserId == userId);
+
+        if (userProfile != null)
+        {
+            _context.Users.Remove(userProfile);
+            await _context.SaveChangesAsync();
         }
 
         var result = await _userManager.DeleteAsync(user);
@@ -113,4 +123,77 @@ public class UserController : ControllerBase
         return BadRequest(new { Message = "User deletion failed." });
     }
 
+    [HttpGet]
+    [Route("get-user-profile/{userId}")]
+    public async Task<ActionResult<User>> GetUserProfile(string userId)
+    {
+        var user = await _context.Users.FirstOrDefaultAsync(u => u.UserId == userId);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        return user;
+    }
+
+    [HttpPut]
+    [Route("update-user-profile/{userId}")]
+    public async Task<IActionResult> UpdateUserProfile(string userId, User user)
+    {
+        if (!string.Equals(userId, user.UserId))
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(user).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!UserExists(userId))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    [HttpPost]
+    [Route("create-user-profile")]
+    public async Task<ActionResult<User>> CreateUserProfile(User user)
+    {
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(nameof(GetUserProfile), new { userId = user.UserId }, user);
+    }
+
+    [HttpGet]
+    [Route("get-roles")]
+    public IActionResult GetRoles()
+    {
+        var roles = _roleManager.Roles.Select(r => r.Name).ToList();
+        return Ok(roles);
+    }
+
+    [HttpGet]
+    [Route("get-all-profiles")]
+    public async Task<ActionResult<IEnumerable<User>>> GetAllUserProfiles()
+    {
+        return await _context.Users.ToListAsync();
+    }
+
+    private bool UserExists(string userId)
+    {
+        return _context.Users.Any(e => e.UserId == userId);
+    }
 }
