6d6cadd25004c0e31babe83f4df802e89c6eca34
Mykola_Vsk <nikolay_vsk@knu.ua>
2024-03-04 23:45:00 +0200

    back complaint system almost finished | should be added: date for complaint model and ?bool - inspection/considered?

--- MapOfActivitiesAPI/Controllers/ComplaintsController.cs
+++ MapOfActivitiesAPI/Controllers/ComplaintsController.cs
@@ -19,6 +19,7 @@ namespace MapOfActivitiesAPI.Controllers
             _context = context;
         }
 
+        
         [HttpGet]
         [Route("all-events-complaints")]
         public async Task<IActionResult> GetEventsComplaints()
@@ -28,7 +29,17 @@ namespace MapOfActivitiesAPI.Controllers
                 return NotFound();
             }
 
-            var complaints = await _context.Complaints.Where(c => c.EventId != null).ToListAsync();
+            var complaints = await _context.Complaints
+                .Where(c => c.EventId != null)
+                .Select(c => new
+                {
+                    c.EventId,
+                    c.Header,
+                    c.Description,
+                    c.AuthorId,
+                    AuthorEmail = _context.Users.FirstOrDefault(u => u.Id == c.AuthorId) != null ? _context.Users.FirstOrDefault(u => u.Id == c.AuthorId).Email : null,
+                })
+                .ToListAsync();
 
             if (complaints == null)
             {
@@ -38,6 +49,36 @@ namespace MapOfActivitiesAPI.Controllers
             return Ok(complaints);
         }
 
+        
+        [HttpGet]
+        [Route("all-users-complaints")]
+        public async Task<IActionResult> GetUsersComplaints()
+        {
+
+            var complaints = await _context.Complaints
+                .Where(c => c.UserId != null)
+                .Select(c => new
+                {
+                    c.Id,
+                    c.Header,
+                    c.Description,
+                    UserId = _context.Users.FirstOrDefault(u => u.Id == c.UserId) != null ? _context.Users.FirstOrDefault(u => u.Id == c.UserId).UserId : null,
+                    UserEmail = _context.Users.FirstOrDefault(u => u.Id == c.UserId) != null ? _context.Users.FirstOrDefault(u => u.Id == c.UserId).Email : null,
+                    AuthorEmail = _context.Users.FirstOrDefault(u => u.Id == c.AuthorId) != null ? _context.Users.FirstOrDefault(u => u.Id == c.AuthorId).Email : null,
+                })
+                .ToListAsync();
+
+            if (complaints == null)
+            {
+                return NoContent();
+            }
+
+            return Ok(complaints);
+        }
+
+
+
+        /*
         [HttpGet]
         [Route("all-users-complaints")]
         public async Task<IActionResult> GetUsersComplaints()
@@ -49,12 +90,47 @@ namespace MapOfActivitiesAPI.Controllers
 
             var complaints = await _context.Complaints.Where(c => c.UserId != null).ToListAsync();
 
+            //var userIds = complaints.Select(c => c.UserId).Distinct().ToList();
+
+
             if (complaints == null)
             {
                 return NoContent();
             }
 
-            return Ok(complaints);
+            return Ok(complaints); // треба додати пошту користувача й автора скарги у відправку на фронтенд
+        }
+        */
+
+        [HttpGet]
+        [Route("user-complaints/{userId}")]
+        public async Task<IActionResult> GetUserComplaints(string userId)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(e => e.UserId == userId);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var userComplaints = await _context.Complaints
+               .Where(c => c.UserId == user.Id)
+               .Select(c => new
+               {
+                   c.Id,
+                   c.Header,
+                   c.Description,
+                   AuthorEmail = _context.Users.FirstOrDefault(u => u.Id == c.AuthorId) != null ? _context.Users.FirstOrDefault(u => u.Id == c.AuthorId).Email : null,
+               })
+               .ToListAsync();
+
+
+            if (userComplaints == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(userComplaints);
         }
 
 
@@ -117,28 +193,28 @@ namespace MapOfActivitiesAPI.Controllers
         //[Authorize(Roles = ApplicationUserRoles.Admin)]
         [HttpPost]
         [Route("event-complaint")]
-        public async Task<ActionResult<Complaint>> PostComplainEvent(string header, string description, int eventId, string userIdAuthor)
+        public async Task<ActionResult<Complaint>> PostComplainEvent(Complaint c)
         {
 
-            var _event = await _context.Events.FirstOrDefaultAsync(e => e.Id == eventId);
+            var _event = await _context.Events.FirstOrDefaultAsync(e => e.Id == c.EventId);
 
             if (_event == null)
             {
                 return NotFound();
             }
 
-            var author = await _context.Users.FirstOrDefaultAsync(e => e.UserId == userIdAuthor);
+            //var author = await _context.Users.FirstOrDefaultAsync(u => u.Id == c.UserId);
 
-            if (author == null)
-            {
-                return NotFound();
-            }
+            //if (author == null)
+            //{
+            //    return NotFound();
+            //}
 
             Complaint myUserComplaint = new Complaint();
-            myUserComplaint.Header = header;
-            myUserComplaint.Description = description;
+            myUserComplaint.Header = c.Header;
+            myUserComplaint.Description = c.Description;
             myUserComplaint.EventId = _event.Id;
-            myUserComplaint.AuthorId = author.Id;
+            myUserComplaint.AuthorId = c.AuthorId;
 
             _context.Complaints.Add(myUserComplaint);
             await _context.SaveChangesAsync();
